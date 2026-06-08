e53af97bd0226e1e7556f83bc0d3c13d06c35110
fareljonathan <fareljonathane2708@gmail.com>
2026-06-08 15:05:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/tentang', function () {
